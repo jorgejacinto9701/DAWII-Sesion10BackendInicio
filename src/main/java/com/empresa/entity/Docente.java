088db777517a7822f7ca55a05be4ac965d2fa4ae
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2022-05-25 23:37:58 -0500

    ajustes en fecha

--- src/main/java/com/empresa/entity/Docente.java
+++ src/main/java/com/empresa/entity/Docente.java
@@ -1,5 +1,7 @@
 package com.empresa.entity;
 
+import java.util.Date;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -7,6 +9,10 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 @Entity
 @Table(name = "docente")
@@ -22,6 +28,19 @@ public class Docente {
 	@ManyToOne
 	@JoinColumn(name = "idUbigeo")
 	private Ubigeo ubigeo;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd hh:mm:ss")
+	private Date fechaRegistro;
+	
+	
+	public Date getFechaRegistro() {
+		return fechaRegistro;
+	}
+
+	public void setFechaRegistro(Date fechaRegistro) {
+		this.fechaRegistro = fechaRegistro;
+	}
 
 	public int getIdDocente() {
 		return idDocente;
